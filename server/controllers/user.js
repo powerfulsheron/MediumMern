5533a94a7f82fdb6e1636842b19ac0dabfce2f8b
Maxime AUBLET <maximeaublet@gmail.com>
2019-07-13 21:25:04 +0200

    bugfix server user update :bug:

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,50 +1,52 @@
-const User = require('../models/user.js');
+const User = require("../models/user.js");
 
 module.exports = {
+  find: (req, res) => {
+    User.find(req.query).then(data => res.json(data));
+  },
 
-    find: (req, res) => {
-        User.find(req.query).then(data => res.json(data));
-    },
-
-    update: (req, res) => {
-        User.findOneAndUpdate( req.body._id, { $set:req.body })
-        .then((updateduser) => {
+  update: (req, res) => {
+    User.findOneAndUpdate({ _id: req.body.id }, { $set: req.body })
+      .then(updateduser => {
         res.status(200).json({
-            success: true,
-            message: 'User updated'
+          success: true,
+          message: "User updated"
         });
-        })
-        .catch((err) => {
+      })
+      .catch(err => {
+        console.log(err);
         res.status(500).json({
-            success: false,
-            message: 'Error when trying to Update User'
+          success: false,
+          message: "Error when trying to update User : " + err
         });
-        });
-    },
- 
-    remove: (req, res) => {
-        if(req.user.id!=req.query._id){
-            res.status(403).send({message:"You are not authorized to do this."});
-        }else{
-            User.findOneAndDelete({ _id:req.query.id })
-            .then((deletedUser)=>{
-                if(deletedUser) {
-                    res.status(200).json({
-                        success: true,
-                        message: 'User deleted.',
-                        deletedUser:deletedUser
-                    });
-                } else {
-                    res.status(202).json({
-                        success: false,
-                        message: 'No User match the query'
-                    });
-                }          
-            }).catch((err) => res.status(500).json({
-                success: false,
-                message: err
-            }));
-        }
-    }
+      });
+  },
 
+  remove: (req, res) => {
+    if (req.user.id != req.query._id) {
+      res.status(403).send({ message: "You are not authorized to do this." });
+    } else {
+      User.findOneAndDelete({ _id: req.query.id })
+        .then(deletedUser => {
+          if (deletedUser) {
+            res.status(200).json({
+              success: true,
+              message: "User deleted.",
+              deletedUser: deletedUser
+            });
+          } else {
+            res.status(202).json({
+              success: false,
+              message: "No User match the query"
+            });
+          }
+        })
+        .catch(err =>
+          res.status(500).json({
+            success: false,
+            message: err
+          })
+        );
+    }
+  }
 };
