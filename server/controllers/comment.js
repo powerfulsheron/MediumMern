e4b967f110a3d9b4e71c4d2ae9e4b2c1405802d3
Maxime AUBLET <maximeaublet@gmail.com>
2019-07-23 00:25:56 +0200

    FIX : Restfull API server

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,58 +1,71 @@
-const Post = require('../models/post.js');
-const User = require('../models/user.js');
+const Post = require("../models/post.js");
+const User = require("../models/user.js");
 
 module.exports = {
-
-    save: (req, res) => {
-        Post.findOne({_id:req.body._idpost}, 'comments').then((post)=>{
-            User.findOne({_id:req.user.id}, 'name surname profilepictureurl').then((user)=>{
-                post.comments.push({
-                    content:req.body.content,
-                    date:req.body.date,
-                    user:{
-                        _id:req.user.id,
-                        name:user.name,
-                        surname:user.surname,
-                        profilepictureurl:user.profilepictureurl
-                    }
-                });
-                post.save();
-                res.status(201).json({
-                    success: true,
-                    message: 'Comment Saved'
-                });
-            }).catch((err) => {
-                console.log('Error when trying to Find Comment User' + err);
+  save: (req, res) => {
+    Post.findOne({ _id: req.body._idpost }, "comments")
+      .then(post => {
+        User.findOne({ _id: req.user.id }, "name surname profilepictureurl")
+          .then(user => {
+            post.comments.push({
+              content: req.body.content,
+              date: req.body.date,
+              user: {
+                _id: req.user.id,
+                name: user.name,
+                surname: user.surname,
+                profilepictureurl: user.profilepictureurl
+              }
             });
-        }).catch((err) => {
-            console.log('Error when trying to Save Comment' + err);
-        });
-    },
-
-    update: (req, res) => {
-        Post.findOne({_id:req.body._idpost}, 'comments').then((post)=>{
-           post.comments.find( comment => comment._id == req.body._idcomment).content=req.body.content;
-           post.save();
-           res.status(200).json({
-                success: true,
-                message: 'Comment Updated'
-            });
-        }).catch((err) => {
-            console.log('Error when trying to Update Comment' + err);
-        });
-    },
- 
-    remove: (req, res) => {
-        Post.findOne({_id:req.query._idpost}, 'comments').then((post)=>{
-            post.comments = post.comments.filter(comment => comment._id != req.query._idcomment);
             post.save();
-            res.status(200).json({
-                 success: true,
-                 message: 'Comment Removed'
-             });
-         }).catch((err) => {
-             console.log('Error when trying to Delete Comment' + err);
-         });
-      }
+            res.status(201).json({
+              success: true,
+              message: "Comment Saved"
+            });
+          })
+          .catch(err => {
+            res.status(404).json({});
+          });
+      })
+      .catch(err => {
+        console.log("Error when trying to save Comment : " + err);
+        res.status(400).json({
+          error: err
+        });
+      });
+  },
 
+  update: (req, res) => {
+    Post.findOne({ _id: req.body._idpost }, "comments")
+      .then(post => {
+        post.comments.find(
+          comment => comment._id == req.body._idcomment
+        ).content = req.body.content;
+        post.save();
+        res.status(200).json({
+          success: true,
+          message: "Comment Updated"
+        });
+      })
+      .catch(err => {
+        console.log("Error when trying to update comment" + err);
+        res.status(400).json({
+          error: err
+        });
+      });
+  },
+
+  remove: (req, res) => {
+    Post.findOne({ _id: req.query._idpost }, "comments")
+      .then(post => {
+        post.comments = post.comments.filter(
+          comment => comment._id != req.query._idcomment
+        );
+        post.save();
+        res.status(204).send();
+      })
+      .catch(err => {
+        console.log("Error when trying to delete comment" + err);
+      });
+  }
 };
--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,119 +1,124 @@
-const Post = require('../models/post.js');
-const User = require('../models/user.js');
-const Type = require('../models/type.js');
+const Post = require("../models/post.js");
+const User = require("../models/user.js");
+const Type = require("../models/type.js");
 
 module.exports = {
+  findAll: (req, res) => {
+    Post.find(req.query).then(data => res.json(data));
+  },
 
-    findAll: (req, res) => {
-        Post.find(req.query).then(data => res.json(data));
-    },
-
-    findByType: (req, res) => {
-        Post.find(req.query, 'title date timetoread score user type').then(data => res.json(data));
-    },
-
-    save: (req, res) => {
-        Type.findOne({_id:req.body.type},'name').then((type)=>{
-            User.findOne({_id:req.user.id}, 'name surname profilepictureurl posts').then((user)=>{
-                var post = new Post(req.body);
-                post.date = new Date();
-                post.type = {
-
-                    _id : req.body.type,
-                    name : type.name
-                }
-                post.user = {
-                    _id : req.user.id,
-                    name : user.name,
-                    surname : user.surname,
-                    profilepictureurl : user.profilepictureurl
-                }
-                post.save().then((savedPost) => { // Middleware mongoose
-                    user.posts.push(
-                        {
-                            _id : savedPost._id,
-                            title : savedPost.title,
-                            timetoread : savedPost.timetoread,
-                            score : savedPost.score
-                        }
-                    );
-                    user.save();
-                    res.status(201).json({
-                        success: true,
-                        message: 'Post Saved',
-                        post:savedPost
-                    });
-                });
-            });
-        }).catch((err) => {
-            res.status(500).json({
-                success: false,
-                message: 'Error when trying to Save Post' + err
-            });
+  findByType: (req, res) => {
+    Post.find(req.query, "title date timetoread score user type")
+      .then(data => res.json(data))
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({
+          error: "Error when trying to Save Post : " + err
         });
-    },
+      });
+  },
 
-    update: (req, res) => {
-        Post.findOneAndUpdate({ _id:req.body._id }, { $set:req.body })
-        .then((updatedpost) => {
-            res.status(200).json({
-                success: true,
-                message: 'Post updated',
-                updatedPost:updatedpost
+  save: (req, res) => {
+    Type.findOne({ _id: req.body.type }, "name")
+      .then(type => {
+        User.findOne(
+          { _id: req.user.id },
+          "name surname profilepictureurl posts"
+        ).then(user => {
+          var post = new Post(req.body);
+          post.date = new Date();
+          post.type = {
+            _id: req.body.type,
+            name: type.name
+          };
+          post.user = {
+            _id: req.user.id,
+            name: user.name,
+            surname: user.surname,
+            profilepictureurl: user.profilepictureurl
+          };
+          post.save().then(savedPost => {
+            // Middleware mongoose
+            user.posts.push({
+              _id: savedPost._id,
+              title: savedPost.title,
+              timetoread: savedPost.timetoread,
+              score: savedPost.score
             });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                success: false,
-                message: 'Error when trying to Update Post'
+            user.save();
+            res.status(201).json({
+              success: true,
+              message: "Post Saved",
+              post: savedPost
             });
+          });
         });
-    },
- 
-    remove: (req, res) => {
-        Post.findOneAndDelete({ _id:req.query._id })
-        .then((deletedPost)=>{
-        if(deletedPost) {
-            res.status(204).json({
-                success: true,
-                message: 'Post deleted.'
-            });
-            } else {
-            res.status(202).json({
-                success: false,
-                message: 'No Post match the query'
-            });
-            }          
-        }
-        )
-        .catch((err) => res.status(500).json({
-            success: false,
-            message: err
-        }));
-    },
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({
+          error: "Error when trying to Save Post : " + err
+        });
+      });
+  },
 
-    incrementScore: (req, res) => {
-        Post.findOneAndUpdate({ _id:req.body._id }, { $inc:{ score: 1 } }).then(()=>{
-            res.status(200).json({
-                success: true,
-                message: 'Score incremented by 1',
-            });       
-        }).catch((err) => res.status(500).json({
-            success: false,
-            message: err
-        }));
-    },
+  update: (req, res) => {
+    Post.findOneAndUpdate({ _id: req.body._id }, { $set: req.body })
+      .then(updatedpost => {
+        if (updatedpost) res.status(200).json(updatedpost);
+        else res.status(404).json({});
+      })
+      .catch(err => {
+        console.err(err);
+        res.status(400).json({
+          error: "Error when trying to update Post" + err
+        });
+      });
+  },
 
-    decrementScore: (req, res) => {
-        Post.findOneAndUpdate({ _id:req.body._id }, { $inc:{ score: -1 } }).then(()=>{
-            res.status(200).json({
-                success: true,
-                message: 'Score decremented by 1',
-            });       
-        }).catch((err) => res.status(500).json({
-            success: false,
-            message: err
-        }));
-    }
+  remove: (req, res) => {
+    Post.findOneAndDelete({ _id: req.query._id })
+      .then(deletedPost => {
+        if (deletedPost) res.status(204).send();
+        else res.status(404).json({});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({ error: err });
+      });
+  },
 
+  incrementScore: (req, res) => {
+    Post.findOneAndUpdate({ _id: req.body._id }, { $inc: { score: 1 } })
+      .then(post => {
+        if (post)
+          res.status(200).json({
+            success: true,
+            message: "Score incremented by 1"
+          });
+        else res.status(404).json({});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({ error: err });
+      });
+  },
+
+  decrementScore: (req, res) => {
+    Post.findOneAndUpdate({ _id: req.body._id }, { $inc: { score: -1 } })
+      .then(post => {
+        if (post)
+          res.status(200).json({
+            success: true,
+            message: "Score decremented by 1"
+          });
+        else res.status(404).json({});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+      });
+  }
 };
